54c6e1144263f1553a332d4997b7fddf638cfb73
John Doe <johndoe@example.com>
2024-11-28 09:30:42 +0800

    1

--- src/config/listen.php
+++ src/config/listen.php
@@ -2,9 +2,7 @@
 
 use App\Php94\Admin\Http\Account\Index as AccountIndex;
 use App\Php94\Admin\Http\App\Index as AppIndex;
-use App\Php94\Admin\Http\Cache\Index as CacheIndex;
 use App\Php94\Admin\Http\Role\Index as RoleIndex;
-
 use App\Php94\Admin\Http\Common;
 use App\Php94\Admin\Middleware\AuthMiddleware;
 use App\Php94\Admin\Model\Menu;
@@ -26,7 +24,6 @@ return [
         $menu->addMenu('角色管理', RoleIndex::class);
         $menu->addMenu('账户管理', AccountIndex::class);
         $menu->addMenu('应用管理', AppIndex::class);
-        $menu->addMenu('缓存管理', CacheIndex::class);
     },
     Widget::class => function (
         Widget $widget
